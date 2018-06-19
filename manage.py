9ac1990f5d0c94541a5475990eb5fa7661ad1de0
lee <475952675@xxx.com>
2018-06-19 21:10:12 +0800

    修改

--- manage.py
+++ manage.py
@@ -19,14 +19,11 @@ class Config(object):
     # redis配置
     REDIS_HOST="127.0.0.1"
     REDIS_PORT =6379
-
     # flask_session的配置信息
     SESSION_TYPE ="redis" #指定session保存到rediszhong
     SESSION_USE_SIGNER =True # 让 cookie 中的 session_id 被加密签名处理
     SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
     PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
-
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -41,9 +38,6 @@ manager.add_command("db",MigrateCommand)
 @app.route("/")
 def demo():
     return "hello world"
-
-
-
 
 if __name__ == "__main__":
     manager.run()
